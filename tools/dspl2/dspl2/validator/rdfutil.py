c817fc1363e5c6e47e2a713d4f87e1a8d82139af
Natarajan Krishnaswami <nkrishnaswami@google.com>
2019-08-19 17:20:01 -0400

    remove ABSL flags from library

--- tools/dspl2/dspl2/validator/rdfutil.py
+++ tools/dspl2/dspl2/validator/rdfutil.py
@@ -17,25 +17,16 @@ _Frame = {
     '@type': 'StatisticalDataset',
 }
 _Module_path = Path(__file__).parent
-
-FLAGS = flags.FLAGS
-flags.DEFINE_string('schema',
-                    (_Module_path / 'schema' / 'schema.jsonld').as_posix(),
-                    'Path to schema.org schema')
-flags.DEFINE_string('context',
-                    (_Module_path / 'schema' / 'jsonldcontext.json').as_posix(),
-                    'Path to schema.org context')
-
 _initialized = False
 
 
-def Init():
+def _Init(context, schema):
   global _Context, _Schema
-  print('Loading schema', file=sys.stderr)
-  with open(FLAGS.schema) as schema:
+  print(f'Loading schema from "{schema}"', file=sys.stderr)
+  with open(schema) as schema:
     _Schema.update(json.load(schema))
-  print('Loading context', file=sys.stderr)
-  with open(FLAGS.context) as context:
+  print(f'Loading context from "{context}"', file=sys.stderr)
+  with open(context) as context:
     _Context.update(json.load(context))
   del _Context['@context']['id']
   del _Context['@context']['type']
@@ -44,7 +35,10 @@ def Init():
 
 def NormalizeJsonLd(json_val):
   if not _initialized:
-    Init()
+    _Init(
+       (_Module_path / 'schema' / 'jsonldcontext.json').as_posix(),
+       (_Module_path / 'schema' / 'schema.jsonld').as_posix(),
+    )
   print('Expanding JSON-LD')
   expanded = jsonld.expand(json_val)
   print('Flattening JSON-LD')
@@ -57,11 +51,11 @@ def NormalizeJsonLd(json_val):
   return framed
 
 
-def main(args):
+def main(args, context, schema):
   with open(args[1]) as f:
     json_val = json.load(f)
   normalized = NormalizeJsonLd(json_val)
-  print(json.dumps(normalized, indent=2))
+  json.dump(normalized, sys.stdout, indent=2)
 
 
 if __name__ == '__main__':
